6f50fb5b30219b60a3854c009066471d99db9341
hovannhan <hovannhan10031997@gmail.com>
2021-02-23 22:32:18 +0700

    create view admin

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminsRouter = require('./routes/admin');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.engine('ejs', require('@express-ejs-extend'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
